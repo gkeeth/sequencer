fe45b143b1c441d71c6df871f78647c24c76cd87
Graham Keeth <graham.keeth@gmail.com>
2023-03-22 21:10:58 -0400

    reduce number of min/max functions

--- src/sequencer/test/utils_test.cpp
+++ src/sequencer/test/utils_test.cpp
@@ -8,49 +8,29 @@ extern "C" {
 TEST_GROUP(utils_testgroup) {};
 
 TEST(utils_testgroup, signed_max) {
-    CHECK_EQUAL(2, max_8(1, 2));
-    CHECK_EQUAL(2, max_8(2, 1));
-    CHECK_EQUAL(2, max_8(-1, 2));
-    CHECK_EQUAL(-1, max_8(-1, -2));
-    CHECK_EQUAL(2, max_16(1, 2));
-    CHECK_EQUAL(2, max_16(2, 1));
-    CHECK_EQUAL(2, max_16(-1, 2));
-    CHECK_EQUAL(-1, max_16(-1, -2));
-    CHECK_EQUAL(2, max_32(1, 2));
-    CHECK_EQUAL(2, max_32(2, 1));
-    CHECK_EQUAL(2, max_32(-1, 2));
-    CHECK_EQUAL(-1, max_32(-1, -2));
+    CHECK_EQUAL(2, max(1, 2));
+    CHECK_EQUAL(2, max(2, 1));
+    CHECK_EQUAL(2, max(-1, 2));
+    CHECK_EQUAL(-1, max(-1, -2));
+    CHECK_EQUAL(INT32_MAX, max(INT32_MIN, INT32_MAX));
 }
 
 TEST(utils_testgroup, signed_min) {
-    CHECK_EQUAL(1, min_8(1, 2));
-    CHECK_EQUAL(1, min_8(2, 1));
-    CHECK_EQUAL(-1, min_8(-1, 2));
-    CHECK_EQUAL(-2, min_8(-1, -2));
-    CHECK_EQUAL(1, min_16(1, 2));
-    CHECK_EQUAL(1, min_16(2, 1));
-    CHECK_EQUAL(-1, min_16(-1, 2));
-    CHECK_EQUAL(-2, min_16(-1, -2));
-    CHECK_EQUAL(1, min_32(1, 2));
-    CHECK_EQUAL(1, min_32(2, 1));
-    CHECK_EQUAL(-1, min_32(-1, 2));
-    CHECK_EQUAL(-2, min_32(-1, -2));
+    CHECK_EQUAL(1, min(1, 2));
+    CHECK_EQUAL(1, min(2, 1));
+    CHECK_EQUAL(-1, min(-1, 2));
+    CHECK_EQUAL(-2, min(-1, -2));
+    CHECK_EQUAL(INT32_MIN, min(INT32_MIN, INT32_MAX));
 }
 
 TEST(utils_testgroup, unsigned_max) {
-    CHECK_EQUAL(2, max_u8(1, 2));
-    CHECK_EQUAL(2, max_u8(2, 1));
-    CHECK_EQUAL(2, max_u16(1, 2));
-    CHECK_EQUAL(2, max_u16(2, 1));
-    CHECK_EQUAL(2, max_u32(1, 2));
-    CHECK_EQUAL(2, max_u32(2, 1));
+    CHECK_EQUAL(2, umax(1, 2));
+    CHECK_EQUAL(2, umax(2, 1));
+    CHECK_EQUAL(UINT32_MAX, umax(0, UINT32_MAX));
 }
 
 TEST(utils_testgroup, unsigned_min) {
-    CHECK_EQUAL(1, min_u8(1, 2));
-    CHECK_EQUAL(1, min_u8(2, 1));
-    CHECK_EQUAL(1, min_u16(1, 2));
-    CHECK_EQUAL(1, min_u16(2, 1));
-    CHECK_EQUAL(1, min_u32(1, 2));
-    CHECK_EQUAL(1, min_u32(2, 1));
+    CHECK_EQUAL(1, umin(1, 2));
+    CHECK_EQUAL(1, umin(2, 1));
+    CHECK_EQUAL(0, umin(0, UINT32_MAX));
 }
--- src/sequencer/utils.h
+++ src/sequencer/utils.h
@@ -7,18 +7,9 @@ void failed(char *file, int line);
 #define ASSERT(expr) \
     if (expr) {} else failed(__FILE__, __LINE__)
 
-uint8_t max_u8(uint8_t a, uint8_t b);
-uint8_t min_u8(uint8_t a, uint8_t b);
-uint16_t max_u16(uint16_t a, uint16_t b);
-uint16_t min_u16(uint16_t a, uint16_t b);
-uint32_t max_u32(uint32_t a, uint32_t b);
-uint32_t min_u32(uint32_t a, uint32_t b);
-
-int8_t max_8(int8_t a, int8_t b);
-int8_t min_8(int8_t a, int8_t b);
-int16_t max_16(int16_t a, int16_t b);
-int16_t min_16(int16_t a, int16_t b);
-int32_t max_32(int32_t a, int32_t b);
-int32_t min_32(int32_t a, int32_t b);
+uint32_t umax(uint32_t a, uint32_t b);
+uint32_t umin(uint32_t a, uint32_t b);
+int32_t max(int32_t a, int32_t b);
+int32_t min(int32_t a, int32_t b);
 
 #endif // UTILS_H
