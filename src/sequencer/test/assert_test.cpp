1ffe6ff275cb1e530c6059fc2d57eb76560b1701
Graham Keeth <graham.keeth@gmail.com>
2023-04-14 08:28:21 -0400

    Split assert tests into their own file

---
+++ src/sequencer/test/assert_test.cpp
@@ -0,0 +1,30 @@
+#include "CppUTest/TestHarness.h"
+
+#include "platform_utils_fake.h"
+
+extern "C" {
+#include "utils.h"
+}
+
+TEST_GROUP(assert_testgroup) {
+    void setup() {
+        assert_fake_setup(false);
+    }
+
+    void teardown() {
+        assert_fake_setup(false);
+    }
+};
+
+TEST(assert_testgroup, assert_pass_test) {
+    CHECK_FALSE(assert_get_expected());
+    ASSERT(1);
+    CHECK_FALSE(assert_get_hit());
+}
+
+TEST(assert_testgroup, assert_fail_test) {
+    assert_fake_setup(true);
+    CHECK_TRUE(assert_get_expected());
+    ASSERT(0);
+    CHECK_TRUE(assert_get_hit());
+}
--- src/sequencer/test/platform_utils_test.cpp
+++ src/sequencer/test/platform_utils_test.cpp
@@ -32,26 +32,3 @@ TEST(platform_utils_testgroup, timer_ms_to_arr_out_of_range) {
     timer_ms_to_arr(3000); // will overflow a 16-bit ARR
     CHECK_TRUE(assert_get_hit());
 }
-
-TEST_GROUP(assert_testgroup) {
-    void setup() {
-        assert_fake_setup(false);
-    }
-
-    void teardown() {
-        assert_fake_setup(false);
-    }
-};
-
-TEST(assert_testgroup, assert_pass_test) {
-    CHECK_FALSE(assert_get_expected());
-    ASSERT(1);
-    CHECK_FALSE(assert_get_hit());
-}
-
-TEST(assert_testgroup, assert_fail_test) {
-    assert_fake_setup(true);
-    CHECK_TRUE(assert_get_expected());
-    ASSERT(0);
-    CHECK_TRUE(assert_get_hit());
-}
