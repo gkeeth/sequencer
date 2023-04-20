d8f4d0d2fdfcc59de67b09aa2e3ccdd787350b8e
Graham Keeth <graham.keeth@gmail.com>
2023-04-19 22:25:49 -0400

    Add tests for tempo_to_period_and_prescaler()

--- src/sequencer/test/platform_utils_test.cpp
+++ src/sequencer/test/platform_utils_test.cpp
@@ -6,6 +6,7 @@ extern "C" {
 #include "utils.h"
 #include "platform_utils.h"
 #include "platform_constants.h"
+#include "sequencer.h" // for MIN/MAX_BPM_TENTHS
 }
 
 TEST_GROUP(platform_utils_testgroup) {
@@ -28,8 +29,19 @@ TEST_GROUP(platform_utils_testgroup) {
         duty_to_pwm_compare(period, duty_percent);
         FAIL("did not hit expected assert in duty_to_pwm_compare");
     }
+
+    void tempo_to_period_and_prescaler_assert(uint32_t tenths_of_bpm) {
+        uint32_t dummy_period;
+        uint32_t dummy_prescaler;
+
+        assert_fake_setup(true);
+        tempo_to_period_and_prescaler(tenths_of_bpm, &dummy_period, &dummy_prescaler);
+        FAIL("did not hit expected assert in tempo_to_period_and_prescaler");
+    }
 };
 
+
+// timer_ms_to_arr()
 TEST(platform_utils_testgroup, timer_ms_to_arr_in_range) {
     uint32_t PERIOD_MS_30_BPM = 60 * 1000 / 30;
     uint32_t PERIOD_MS_300_BPM = 60 * 1000 / 300;
@@ -53,7 +65,6 @@ TEST(platform_utils_testgroup, timer_ms_to_arr_assert_psc_out_of_range_max) {
 }
 
 TEST(platform_utils_testgroup, timer_ms_to_arr_assert_overflow) {
-    // fail the overflow-prevention assert
     timer_ms_to_arr_expect_assert(UINT32_MAX, 1);
 }
 
@@ -61,6 +72,41 @@ TEST(platform_utils_testgroup, timer_ms_to_arr_assert_too_big) {
     timer_ms_to_arr_expect_assert(2, 1);
 }
 
+
+// tempo_to_period_and_prescaler()
+TEST(platform_utils_testgroup, tempo_to_period_and_prescaler_max_bpm) {
+    tempo_to_period_and_prescaler_assert(MAX_BPM_TENTHS + 1U);
+}
+
+TEST(platform_utils_testgroup, tempo_to_period_and_prescaler_min_bpm) {
+    tempo_to_period_and_prescaler_assert(MIN_BPM_TENTHS - 1U);
+}
+
+TEST(platform_utils_testgroup, tempo_to_period_and_prescaler_ranges) {
+    uint32_t bpms[] = {2001, 1001, 501, 301};
+    uint32_t period = 0;
+    uint32_t prescaler = 0;
+
+    for (size_t i = 0; i < sizeof(bpms) / sizeof(bpms[0]); ++i) {
+        tempo_to_period_and_prescaler(bpms[i], &period, &prescaler);
+        uint32_t bpm = (uint64_t) SYSCLK_FREQ_HZ * 60U * 10U / (prescaler * period);
+        CHECK_EQUAL(bpms[i], bpm);
+    }
+}
+
+TEST(platform_utils_testgroup, tempo_to_period_and_prescaler_check_all_bpms) {
+    uint32_t period = 0;
+    uint32_t prescaler = 0;
+
+    for (uint32_t dbpm = MIN_BPM_TENTHS; dbpm <= MAX_BPM_TENTHS; ++dbpm) {
+        tempo_to_period_and_prescaler(dbpm, &period, &prescaler);
+        double actual = (double) SYSCLK_FREQ_HZ * 60.0 * 10.0 / (prescaler * period);
+        DOUBLES_EQUAL(dbpm, actual, 0.09);
+    }
+}
+
+
+// duty_to_pwm_compare()
 TEST(platform_utils_testgroup, duty_to_pwm_compare_correct_calculation) {
     CHECK_EQUAL(1000, duty_to_pwm_compare(1000, 100));
     CHECK_EQUAL(990, duty_to_pwm_compare(1000, 99));
